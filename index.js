d4de3cecf4f9dc96d9827de8ac101fdb65671d39
masontang@aol.com <masontang@aol.com>
2018-06-27 17:50:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+let a = 5
+let b = 6
+let n = 7
+
+function add(a,b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function inc(n){
+  return n += 1
+}
+
+function dec(n){
+  return n -= 1
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
+
